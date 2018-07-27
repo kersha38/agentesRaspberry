e86809f9d6d574160bf423799e80cb302a2d744c
kersha38 <caralextan@hotmail.com>
2018-07-26 23:47:09 -0500

    redes y sensores

--- pruebas.py
+++ pruebas.py
@@ -1,1 +1,12 @@
-import requests
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(29,GPIO.IN)#,pull_up_down=GPIO.PUD_UP)
+
+try:
+    while(True):
+        print("midiento")
+        print(GPIO.input(29))
+        time.sleep(1)
+finally:
+    GPIO.cleanup()
--- sensor.py
+++ sensor.py
@@ -1,18 +1,27 @@
 import RPi.GPIO as GPIO
+import time
 
-idSensorAgua=40
-idSensorComida=38
+idSensorAgua=31
+idSensorComida=29
 idSensorLuz=40
 
 def iniciar_sensores():
     print("iniciando sensores")
-    # GPIO.setmode(GPIO.BCM)
+    # GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(idSensorComida,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(idSensorAgua,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
 def hay_agua():
-    return "SI"
+    if(GPIO.inpu(idSensorAgua)):
+        return "SI"
+    else:
+        return "NO"
 
 def hay_comida():
-    return "NO"
+    if(GPIO.input(idSensorComida)):
+        return "SI"
+    else:
+        return "NO"
 
 def estado_luz():
     if(GPIO.input(idSensorLuz)):
@@ -24,4 +33,8 @@ def estado_luz():
 def finalizar_sensores():
     print("finalizar sensores")
 
+def obtenerHora():
+    return time.strftime("%H:%M:%S")
 
+def obtenerFecha():
+    return time.strftime("%d/%m/%y")
